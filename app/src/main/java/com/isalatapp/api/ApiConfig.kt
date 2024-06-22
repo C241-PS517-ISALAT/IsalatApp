156a0f7acf0913c98d9af7245a530e5b0db428dd
BagusCPaste <muhbagussa@gmail.com>
2024-06-22 11:26:48 +0700

    fix eror

--- app/src/main/java/com/isalatapp/api/ApiConfig.kt
+++ app/src/main/java/com/isalatapp/api/ApiConfig.kt
@@ -6,6 +6,4 @@ import com.isalatapp.BuildConfig
 object ApiConfig {
     const val BASE_URL = BuildConfig.NEWS_API_URL
     const val API_KEY = BuildConfig.NEWS_API_KEY
-    const val GITHUB_TOKEN = BuildConfig.TOKEN_GITHUB
-
 }
